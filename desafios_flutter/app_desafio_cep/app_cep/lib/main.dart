3b4c8c4e4931516ec99656567e15bcabe97297f2
paulohenriquejustino <phjustino125@gmail.com>
2024-06-24 16:17:15 -0300

    atualizando

--- desafios_flutter/app_desafio_cep/app_cep/lib/main.dart
+++ desafios_flutter/app_desafio_cep/app_cep/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:trilhaapp/pages/consulta_cep.dart';
-
-
 void main() {
   runApp(const CepPageBack4App());
 }
--- desafios_flutter/app_desafio_cep/app_cep/lib/repository/back4app/cep_repository_back4app.dart
+++ desafios_flutter/app_desafio_cep/app_cep/lib/repository/back4app/cep_repository_back4app.dart
@@ -8,7 +8,6 @@ import 'package:http/http.dart' as http;
 class CepBack4AppRepository {
   // Criando um Dio.
   final _dio = Dio();
-
   // Configuração inicial do repositório
   CepBack4AppRepository() {
     // Configura o cabeçalho de autenticação
